a8fbdfc8a9eb16e7de3d93d17e1c8438761fd179
Tabriz <nomaliev04@gmail.com>
2025-02-24 01:33:00 +0400

    dependency injection

--- lib/core/logic/auth_cubic.dart
+++ lib/core/logic/auth_cubic.dart
@@ -1,50 +1,9 @@
+import 'package:coffeeapp/core/data/repositories/auth_repository.dart';
 import 'package:coffeeapp/core/logic/auth_state.dart';
-import 'package:coffeeapp/core/utils/exceptions/firebase_auth_exceptions.dart';
-import 'package:coffeeapp/core/utils/exceptions/firebase_exceptions.dart';
-import 'package:coffeeapp/core/utils/exceptions/platform_exceptions.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_storage/get_storage.dart';
 
 class AuthCubit extends Cubit<AuthCubitState> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final AuthRepository _authRepository;
 
-  AuthCubit() : super(AuthInitial());
-
-  User? get authUser => _auth.currentUser;
-
-  void checkAuthStatus() {
-    final user = _auth.currentUser;
-    if (user != null) {
-      if (user.emailVerified) {
-      } else {}
-    } else {
-      final deviceStorage = GetStorage();
-      deviceStorage.writeIfNull('FIRSTTIME', true);
-    }
-  }
-
-  Future<UserCredential> registerWithEmailAndPassword(
-    String email,
-    String password,
-  ) async {
-    try {
-      final user = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return user;
-    } on FirebaseAuthException catch (e) {
-      throw AppFirebaseAuthException(e.code).message;
-    } on FirebaseException catch (e) {
-      throw AppFirebaseException(e.code).message;
-    } on FormatException catch (_) {
-      throw const FormatException();
-    } on PlatformException catch (e) {
-      throw AppPlatformException(e.code).message;
-    } catch (e) {
-      throw 'Something went wrong. Please try again';
-    } finally {}
-  }
+  AuthCubit(this._authRepository) : super(AuthInitial());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:coffeeapp/app.dart';
+import 'package:coffeeapp/core/data/repositories/auth_repository.dart';
+import 'package:coffeeapp/core/logic/auth_cubic.dart';
 import 'package:coffeeapp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
+  final authRepository = AuthRepository();
   await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthCubit>(create: (context) => AuthCubit(authRepository)),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
